a53c548effee7bcb72a46bc1d40a6b72102d2bea
digitag62 <144676268+digitag62@users.noreply.github.com>
2025-03-20 07:59:30 +0000

    update: sidebar link use next link

--- next-15/app/(dashboard)/dashboard/page.tsx
+++ next-15/app/(dashboard)/dashboard/page.tsx
@@ -9,7 +9,7 @@ const DashboardPage = async () => {
     <div className="min-h-screen flex justify-center items-center">
       <div className="flex flex-col items-center justify-center gap-2">
         <h1 className="text-xl">Dashboard</h1>
-        <p>Yo, welcome {session?.user?.email?.split("@")[0]}</p>
+        <p>Yo, welcome {session?.user?.email?.split("@")[0]!}</p>
       </div>
     </div>
   );
